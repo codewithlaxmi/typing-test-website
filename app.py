40db5d9bbd26d63ad179aa83a46080a29f83aa8b
Laxmi <laxmiprajapat221242@gmail.com>
2026-05-18 00:12:48 +0530

    First upload

---
+++ app.py
@@ -0,0 +1,315 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+
+tasks = [
+    {
+        "title": "Basic words",
+        "level": "Easy level",
+        "textIcon": "ABC",
+        "number": "01",
+        "bg": "from-slate-900 to-slate-800",
+        "levelColor": "bg-green-500/20 text-green-400"
+    },
+
+    {
+        "title": "Easy sentences",
+        "level": "Easy level",
+        "icon": 'fa-solid fa-comment-dots',
+        "number": "06",
+        "bg": "from-slate-500/20 to-slate-800",
+        "levelColor": "bg-green-500/20 text-green-400",
+        "iconColor": "text-cyan-400"
+    },
+
+    {
+        "title": "Big words",
+        "level": "Easy level",
+        "icon": " fa-solid fa-book-open ",
+        "number": "08",
+        "bg": "from-slate-500/20 to-slate-800",
+        "levelColor": "bg-green-500/20 text-green-400",
+        "iconColor":"text-orange-400"
+    },
+    
+    {
+        "title":"Paragraph",
+        "level":"Easy level",
+        "icon": "fa-solid fa-file-lines",
+        "number":"04",
+        "bg": "from-slate-900 to-slate-800",
+        "levelColor":"bg-green-500/20 text-green-400",
+        "iconColor":"text-pink-400"
+    },
+
+    {
+        "title":"easy lines",
+        "level":"Easy level",
+        "icon":"fa-solid fa-file-lines",
+        "number":"05" ,
+       "bg": "from-slate-900 to-slate-800",
+        "levelColor":"bg-green-500/20 text-green-400",
+        "iconColor":"text-green-200"
+           
+   },
+
+      {
+        "title":"Speed test",
+        "level":"Easy level",
+        "icon":"fa-solid fa-gauge-high",
+        "number":"06" ,
+        "bg": "from-slate-900 to-slate-800",
+        "levelColor":"bg-green-500/20 text-green-400",
+        "iconColor":"text-sky-500"
+           
+   },
+
+   {
+       "title":"Speed test ",
+       "level":"Medium",
+       "textIcon":"123",
+       "number":"07",
+        "bg": "from-slate-900 to-slate-800",
+       "levelColor":"bg-orange-500/20 text-orange-400",
+       "textColor": "text-green-500"
+   },
+
+   {
+        "title":"Accuracy",
+        "level":"Medium",
+         "levelColor": "bg-orange-500/20 text-orange-400",
+        "icon": "fa-solid fa-bullseye ",
+        "number": "08",
+         "bg": "from-slate-900 to-slate-800",
+        "levelColor":"bg-orange-300/20 text-orange-400",
+        "iconColor":"text-red-400"
+
+   },
+
+   {
+       "title":"Symbol Practice",
+       "level":"Medium",
+       "levelColor":"bg-orange-500/20 text-orange-400",
+       "icon":"fa-solid fa-at",
+       "number":"09",
+       "bg": "from-slate-900 to-slate-800",
+       "levelColor":"bg-orange-300/20 text-orange-400",
+       "iconColor": "text-yellow-500"
+   },
+
+   {
+       
+       "title":"Capital Letters",
+       "level":"Medium",
+       "levelColor":"bg-orange-500/20 text-orange-400",
+       "icon":"fa-solid fa-arrow-up-a-z",
+       "number":"09",
+       "bg": "from-slate-900 to-slate-800",
+       "levelColor":"bg-orange-300/20 text-orange-400",
+       "iconColor": "text-lime-100"
+      
+   },
+
+   {
+        "title":"Quick Fingers",
+       "level":"Medium",
+       "levelColor":"bg-orange-500/20 text-orange-400",
+       "icon":"fa-solid fa-hand-sparkles",
+       "number":"09",
+       "bg": "from-slate-900 to-slate-800",
+       "levelColor":"bg-orange-300/20 text-orange-400",
+       "iconColor": "text-fuchsia-200"
+       
+   },
+
+    {
+        "title":"Reaction Test",
+       "level":"Medium",
+       "levelColor":"bg-orange-500/20 text-orange-400",
+       "icon":"fa-solid fa-stopwatch",
+       "number":"09",
+       "bg": "from-slate-900 to-slate-800",
+       "levelColor":"bg-orange-300/20 text-orange-400",
+       "iconColor": "text-cyan-300"
+    },
+
+    {
+        "title":"Speed Burst",
+       "level":"Challenge",
+       "icon":"fa-solid fa-rocket",
+       "number":"09",
+       "bg": "from-slate-900 to-slate-800",
+       "levelColor":"bg-sky-500/20 text-sky-400",
+       "iconColor": "text-orange-300"
+    },
+    {
+        "title":"Blind Typing",
+       "level":"challenge",
+       "icon":"fa-solid fa-eye-slash",
+       "number":"09",
+       "bg": "from-slate-900 to-slate-800",
+       "levelColor":"bg-sky-300/20 text-sky-400",
+       "iconColor": "text-lime-400"
+    },
+
+    {
+        "title":"Focus Sprint",
+       "level":"challenge",
+       "icon":"fa-solid fa-person-running",
+       "number":"09",
+       "bg": "from-slate-900 to-slate-800",
+       "levelColor":"bg-violet-300/20 text-violet-400",
+       "iconColor": "text-lime-100"
+    },
+    {
+        "title":"Instant Type",
+       "level":"challenge",
+       "levelColor":"bg-orange-500/20 text-orange-400",
+       "icon":"fa-solid fa-zap",
+       "number":"09",
+       "bg": "from-slate-900 to-slate-800",
+       "levelColor":"bg-orange-300/20 text-orange-400",
+       "iconColor": "text-sky-400"
+    },
+
+    {
+         "title":"Smooth Typing",
+       "level":"challenge",
+       "levelColor":"bg-orange-500/20 text-orange-400",
+       "icon":"fa-solid fa-water",
+       "number":"09",
+       "bg": "from-slate-900 to-slate-800",
+       "levelColor":"bg-orange-300/20 text-orange-400",
+       "iconColor": "text-sky-100"
+    },
+
+    {
+         "title":"Mirror Mode",
+       "level":"challenge",
+       "levelColor":"bg-orange-500/20 text-orange-400",
+       "icon":"fa-solid fa-arrows-left-right",
+       "number":"09",
+       "bg": "from-slate-900 to-slate-800",
+       "levelColor":"bg-orange-300/20 text-orange-400",
+       "iconColor": "text-sky-400",
+
+    },
+
+    {
+      "title":"Ghost Typing",
+       "level":"challenge",
+       "levelColor":"bg-orange-500/20 text-orange-400",
+       "icon":"fa-solid fa-ghost",
+       "number":"09",
+       "bg": "from-slate-900 to-slate-800",
+       "levelColor":"bg-orange-300/20 text-orange-400",
+       "iconColor": "text-sky-100"
+
+    },
+
+    {
+      "title":"Flash Words",
+       "level":"challenge",
+       "levelColor":"bg-orange-500/20 text-orange-400",
+       "icon":"fa-solid fa-bolt-lightning",
+       "number":"09",
+       "bg": "from-slate-900 to-slate-800",
+       "levelColor":"bg-orange-300/20 text-orange-400",
+       "iconColor": "text-yellow-100"
+
+    },
+
+    {
+    
+      "title":"Spy Mission",
+       "level":"challenge",
+       "levelColor":"bg-orange-500/20 text-orange-400",
+       "icon":"fa-solid fa-user-secret",
+       "number":"09",
+       "bg": "from-slate-900 to-slate-800",
+       "levelColor":"bg-orange-300/20 text-orange-400",
+       "iconColor": "text-red-200"
+
+    
+    },
+
+    {
+      "title":"Lava Floor",
+       "level":"challenge",
+       "levelColor":"bg-orange-500/20 text-orange-400",
+       "icon":"fa-solid fa-fire",
+       "number":"09",
+       "bg": "from-slate-900 to-slate-800",
+       "levelColor":"bg-orange-300/20 text-orange-400",
+       "iconColor": "text-orange-500"
+
+    },
+
+    {
+      "title":"Ice Mode",
+       "level":"challenge",
+       "levelColor":"bg-orange-500/20 text-orange-400",
+       "icon":"fa-solid fa-snowflake",
+       "number":"09",
+       "bg": "from-slate-900 to-slate-800",
+       "levelColor":"bg-orange-300/20 text-orange-400",
+       "iconColor": "text-sky-700"
+
+    },
+
+    {
+      "title":"Zombie Keys",
+       "level":"challenge",
+       "levelColor":"bg-orange-500/20 text-orange-400",
+       "icon":"fa-solid fa-skull",
+       "number":"09",
+       "bg": "from-slate-900 to-slate-800",
+       "levelColor":"bg-orange-300/20 text-orange-400",
+       "iconColor": "text-white"
+
+    }
+
+]
+
+@app.route("/")
+def typing():
+    return render_template("index.html")
+
+@app.route("/practice")
+def practice():
+    return render_template("Practice.html" , tasks=tasks)
+
+@app.route("/history")
+def history():
+    return render_template("History.html")
+
+@app.route("/games")
+def achievements():
+    return render_template("Games.html")
+
+@app.route("/stats")
+def stats():
+    return render_template("stats.html")
+
+
+
+  
+@app.route("/review")
+def review():
+    return render_template("review.html")
+
+
+
+@app.route("/howToPlay")
+def howToPlay():
+    return render_template("howToPlay.html")
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,286 @@
+// ELEMENTS SELECT=================
+
+const startBtn = document.getElementById("startBtn");
+
+const text = document.getElementById("text");
+
+const dropdownBtn = document.getElementById("dropdownBtn");
+
+const dropdownMenu = document.getElementById("dropdownMenu");
+
+const options = document.querySelectorAll(".option");
+
+const timeText = document.getElementById("timeText");
+
+const wpmText = document.getElementById("wpmText");
+
+const errorText = document.getElementById("errorText");
+
+const accuracyText = document.getElementById("accuracyText");
+
+const refreshBtn = document.getElementById("refreshBtn");
+
+const reviewBtn = document.getElementById("reviewBtn");
+
+const howToPlay = document.getElementById("howToPlay");
+
+// VARIABLES=============
+
+//accuracy 
+let accuracy=0;
+
+//wrong letters
+let errors = 0;
+
+//correct letters
+let correct = 0;
+
+//typed letters
+let typedChars = 0;
+
+// typing start time
+let startTime;
+
+let gameStart = false;
+
+let index = 0;
+
+let timer;
+
+let time = parseInt(timeText.innerText);
+
+// PARAGRAPH SETUP================
+
+const para = `Technology has changed the way people learn, communicate, and work in everyday life. Typing is now an important skill for students, developers, writers, and office workers around the world. Improving typing speed requires regular practice, focus, and patience. A good typing habit can help reduce mistakes and increase productivity while working on computers. Many people start slowly, but with consistent effort they become faster and more accurate over time. In coding carrer typing is very the important part because of speed  keep learning`;
+text.innerHTML = "";
+
+para.split("").forEach(function (char) {
+  text.innerHTML += `<span class="text-gray-500">${char}</span>`;
+});
+
+// ALL SPANS===================
+
+const span = text.querySelectorAll("span");
+
+// FIRST CURSOR=================
+
+span[index].classList.add("border-b-2", "border-yellow-400");
+
+// DROPDOWN LOGIC=================
+
+options.forEach(function (option) {
+  option.addEventListener("click", function () {
+    // button text change
+    dropdownBtn.innerText = option.innerText;
+
+    // timer text change
+    timeText.innerText = option.innerText.match(/\d+/)[0] + "s";
+
+    // update time variable
+    time = parseInt(option.innerText.match(/\d+/)[0]);
+
+    // hide dropdown
+    dropdownMenu.classList.add("hidden");
+  });
+});
+
+// open close dropdown
+
+dropdownBtn.addEventListener("click", function () {
+  dropdownMenu.classList.toggle("hidden");
+});
+
+// START BUTTON LOGIC==========
+
+startBtn.addEventListener("click", function () {
+  gameStart = true;
+
+  //save start time
+  startTime = new Date().getTime();
+
+  startBtn.style.display = "none";
+
+  // TIMER START=============
+
+  timer = setInterval(function () {
+    time--;
+
+    timeText.innerText = time + "s";
+
+    // time over
+
+    if (time <= 0) {
+      clearInterval(timer);
+
+      gameStart = false;
+    }
+  }, 1000);
+});
+
+// TYPING EVENT=========
+
+document.addEventListener("keydown", function (e) {
+  // game start check
+  if (gameStart === false) {
+    return;
+  }
+
+  // ignore special keys
+
+  if (
+    e.key === "Shift" ||
+    e.key === "Ctrl" ||
+    e.key === "Alt" ||
+    e.key === "Tab" ||
+    e.key === "CapsLock" ||
+    e.key === "Control"
+  ) {
+    return;
+  }
+
+  // stop page scroll on space
+
+  if (e.key === " ") {
+    e.preventDefault();
+  }
+
+  // stop when complete
+
+  if (index >= span.length) {
+    clearInterval(timer);
+
+    return;
+  }
+
+  //BACKSPACE LOGIC ===============
+  if (e.key === "Backspace") {
+    if (index > 0) {
+      // remove old cursor
+      span[index].classList.remove("border-b-2", "border-yellow-400");
+
+      // move back
+      index--;
+
+     typedChars--;
+
+
+      // check wrong letter
+      if (span[index].classList.contains("text-red-500")) {
+        errors--;
+
+        errorText.innerText = errors;
+      }
+
+      // reset color
+      span[index].classList.remove("text-white", "text-red-500");
+
+      // gray color
+      span[index].classList.add("text-gray-500");
+
+      // cursor back
+      span[index].classList.add("border-b-2", "border-yellow-400");
+    }
+
+    return;
+  }
+
+  // ADD THIS
+typedChars++;
+
+
+ 
+  // CORRECT / WRONG TYPING=======
+
+  if (e.key === span[index].innerText) {
+    //increase correct letters
+    correct++;
+
+    span[index].classList.remove("text-gray-500");
+
+    span[index].classList.add("text-white");
+  } else {
+    // wrong
+    errors++;
+
+    //show errors box update
+    errorText.innerText = errors;
+
+    span[index].classList.remove("text-gray-500");
+
+    span[index].classList.add("text-red-500");
+  }
+
+  // remove old cursor
+
+  span[index].classList.remove("border-b-2", "border-yellow-400");
+
+  // next letter
+
+  index++;
+
+  // next cursor
+
+  if (index < span.length) {
+    span[index].classList.add("border-b-2", "border-yellow-400");
+  }
+
+  //WPM LOGIC=================
+
+  let correctTime = new Date().getTime();
+
+  //total time in minutes
+  let minutes = (correctTime - startTime) / 1000 / 60;
+
+  //total words
+  let words = correct / 5;
+
+  //final wpm
+  let finalWpm = Math.round(words / minutes);
+
+  //avoid infinity
+  if (minutes > 0) {
+    wpmText.innerText = finalWpm;
+  }
+
+
+  //ACCURACY LOGIC===================
+  accuracy = Math.round((correct/typedChars)*100);
+
+  //avoid NaN
+  if(typedChars > 0){
+
+    accuracyText.innerText= accuracy + "%";
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+//REFRESH BUTTON LOGIC==================
+  refreshBtn.addEventListener("click", function(){
+    location.reload();
+  });
+
+  //REVIEW BUTTON LOGIC====================
+  reviewBtn.addEventListener("click",function(){
+    window.location.href="/review";
+  })
+
+ 
